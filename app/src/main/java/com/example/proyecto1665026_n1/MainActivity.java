1fa324fd2b9b4a1fe1576860eef7788e02acf59a
Yahave Mondragon <yahavemondragon@gmail.com>
2021-01-15 14:28:30 -0600

    App alexa ejemplo sencillo del desarrollo de la app, 3 botones hora, dia, chiste

--- app/src/main/java/com/example/proyecto1665026_n1/MainActivity.java
+++ app/src/main/java/com/example/proyecto1665026_n1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.proyecto1665026_n1;
 
+import android.app.Activity;
 import android.speech.tts.TextToSpeech;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,27 +8,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Locale;
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
     private TextToSpeech mTTS;
-    private EditText mEditText;
+    private EditText txthablar;
     private SeekBar mSeekBarPitch;
     private SeekBar mSeekBarSpeed;
     private Button mButtonSpeak;
+
+    public String echo = "eco,"; //Alexa, eco, amazon
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButtonSpeak = findViewById(R.id.button_speak);
+        initControl();
+
+        mButtonSpeak = findViewById(R.id.btnhablar);
         mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
-                    int result = mTTS.setLanguage(Locale.GERMAN);
+                    Locale locale = new Locale("spa", "MEX");// Español mexico
+                    int result = mTTS.setLanguage(locale);
                     if (result == TextToSpeech.LANG_MISSING_DATA
                             || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                         Log.e("TTS", "Language not supported");
@@ -39,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        mEditText = findViewById(R.id.edit_text);
+        txthablar = findViewById(R.id.txthablar);
         mSeekBarPitch = findViewById(R.id.seek_bar_pitch);
         mSeekBarSpeed = findViewById(R.id.seek_bar_speed);
         mButtonSpeak.setOnClickListener(new View.OnClickListener() {
@@ -48,9 +54,12 @@ public class MainActivity extends AppCompatActivity {
                 speak();
             }
         });
-    }
+    }//onCreate
+
+
     private void speak() {
-        String text = mEditText.getText().toString();
+        String text = txthablar.getText().toString();
+        text = echo + text;
         float pitch = (float) mSeekBarPitch.getProgress() / 50;
         if (pitch < 0.1) pitch = 0.1f;
         float speed = (float) mSeekBarSpeed.getProgress() / 50;
@@ -59,6 +68,19 @@ public class MainActivity extends AppCompatActivity {
         mTTS.setSpeechRate(speed);
         mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
+
+    private void speak2(String text) {
+        //String text = txthablar.getText().toString();
+        text = echo + text;
+        float pitch = (float) mSeekBarPitch.getProgress() / 50;
+        if (pitch < 0.1) pitch = 0.1f;
+        float speed = (float) mSeekBarSpeed.getProgress() / 50;
+        if (speed < 0.1) speed = 0.1f;
+        mTTS.setPitch(pitch);
+        mTTS.setSpeechRate(speed);
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
     @Override
     protected void onDestroy() {
         if (mTTS != null) {
@@ -66,5 +88,32 @@ public class MainActivity extends AppCompatActivity {
             mTTS.shutdown();
         }
         super.onDestroy();
+    }
+
+    private void initControl() {
+        ImageButton btnhora = (ImageButton) findViewById(R.id.btnhora);
+        btnhora.setOnClickListener(this);
+
+        ImageButton btndia = (ImageButton) findViewById(R.id.btndia);
+        btndia.setOnClickListener(this);
+
+        Button btnchiste = (Button) findViewById(R.id.btnchiste);
+        btnchiste.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnhora:
+                speak2("¿Que hora es?");
+                break;
+            case R.id.btndia:
+                speak2("¿Que dia es hoy?");
+                break;
+            case R.id.btnchiste:
+                speak2("Cuentame un chiste");
+                break;
+        }
     }
 }
